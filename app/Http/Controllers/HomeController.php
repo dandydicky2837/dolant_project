664701ccbe8c938880b877d65b967bd334a8d0ee
lamkhil <43593728+lamkhil@users.noreply.github.com>
2022-12-29 19:20:55 +0700

    search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,22 +25,26 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-       
-        if ($request->has('search')){
-            if (Auth::user()->role == 1){
-            return view('home',['task'=>task::where('name','LIKE','%'.$request['search'].'%')->where('name', '=', Auth::user()->id)->paginate(15)]);
-        }
-        else return view('admin',['task'=>task::where('name','LIKE','%'.$request['search'].'%')->paginate(15)]);
-        }
-        else{
-           if (Auth::user()->role == 1){
-            return view('home',['task'=>task::where('name', '=', Auth::user()->id)->paginate(15)]);
-        }
-        else return view('admin',['task'=>task::paginate(15)]);
 
+        if ($request->has('search')) {
+            if (Auth::user()->role == 1) {
+                return view('home', ['task' => task::where('user_id', '=', Auth::user()->id)
+                    ->where('jenis_pekerjaan', 'LIKE', '%' . $request['search'] . '%')
+                    ->orWhere('nama_channel', 'LIKE', '%' . $request['search'] . '%')
+                    ->orWhere('judul_seri', 'LIKE', '%' . $request['search'] . '%')
+                    ->orWhere('keterangan_kerja', 'LIKE', '%' . $request['search'] . '%')
+                    ->orWhere('link', 'LIKE', '%' . $request['search'] . '%')->paginate(15)]);
+            } else return view('admin', ['task' => task::where('jenis_pekerjaan', 'LIKE', '%' . $request['search'] . '%')
+                ->orWhere('nama_channel', 'LIKE', '%' . $request['search'] . '%')
+                ->orWhere('judul_seri', 'LIKE', '%' . $request['search'] . '%')
+                ->orWhere('keterangan_kerja', 'LIKE', '%' . $request['search'] . '%')
+                ->orWhere('link', 'LIKE', '%' . $request['search'] . '%')->paginate(15)]);
+        } else {
+            if (Auth::user()->role == 1) {
+                return view('home', ['task' => task::where('user_id', '=', Auth::user()->id)->paginate(15)]);
+            } else return view('admin', ['task' => task::paginate(15)]);
         }
-        
+
         return view('/home');
     }
-    
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -85,9 +85,9 @@
   <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand"></a>
-      <form class="d-flex" role="search" action="/home/search" method="GET">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="d-flex" role="search" action="/home">
+        <input class="form-control me-2" type="text" name="search" placeholder="Search" aria-label="Search">
+        <input class="btn btn-outline-success" type="submit" value="Search">
       </form>
     </div>
   </nav> 
